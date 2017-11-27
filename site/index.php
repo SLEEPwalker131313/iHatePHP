a01b076cd065169f7e79eac8d9938eea030b8758
Unknown <petrunin-vn@yandex.ru>
2017-11-27 23:22:30 +0300

    Debugging

--- site/index.php
+++ site/index.php
@@ -128,7 +128,7 @@
       // https://developer.github.com/v3/auth/
       // https://github.com/blog/1509-personal-api-tokens
       // https://github.com/settings/tokens
-      $access = 'tenminutesgit:10minutesgitpassword';
+      $access = '';
 
       curl_setopt($ch, CURLOPT_URL, $url);
       //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
