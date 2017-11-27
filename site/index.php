6e0969b64536f4386bbf64f181090674c74508ee
Unknown <petrunin-vn@yandex.ru>
2017-11-27 23:40:20 +0300

    Debugging

--- site/index.php
+++ site/index.php
@@ -149,8 +149,8 @@
     //
     // $events = github_request('https://api.github.com/users/:username/events/public?page=1&per_page=5');
     // echo $events;
-    $commits = github_request('https://api.ds');
-    echo $commits;
+    $commits = github_request('https://api.github.com/repos/SLEEPwalker131313/iHatePHP/commits');
+    print_r($commits);
     // $feeds = github_request('https://api.github.com/feeds/:username?page=1&per_page=5');
     // echo $feeds;
     ?>');
