a6a4a39f16be4610fd9ed3ffeb9c29972b6f8e79
Unknown <petrunin-vn@yandex.ru>
2017-11-28 17:24:37 +0300

    Debugging

--- site/index.php
+++ site/index.php
@@ -97,7 +97,7 @@
    $('#gitBtn').on("click", function(){
 
      var formInputValue = $('#commitsInput').val();
-     if(!isNaN(formInputValue) &&  isFinite(formInputValue) && Math.floor(formInputValue) > 0 && Math.floor(formInputValue) < 31) {
+     if(!isNaN(formInputValue) && isFinite(formInputValue) && Math.floor(formInputValue) > 0 && Math.floor(formInputValue) < 31) {
           // console.log(
 //     function github_request($url)
 // {
